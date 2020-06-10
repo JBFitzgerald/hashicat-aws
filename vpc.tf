a17f4798064b65a57e341d6e5b0f62ace83ef64e
Jenny Fitzgerald <jenny.b.fitzgerald@gmail.com>
2020-06-10 18:34:56 +0000

    updated VPC module. x3

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,10 @@
 module "vpc" {
   source  = "app.terraform.io/JFITZGERALD-training/vpc/aws"
   version = "2.39.0"
-
-   tags = {
-    Billable = "true"
-    Department = "devops"
-  }
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+    tags = {
+        Billable = "true"
+        Department = "devops"
+    }
 }
